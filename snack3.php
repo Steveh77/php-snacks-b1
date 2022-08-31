cbfa381eb41b4228c7834c915d34fc81f7751d2c
Steveh77 <stederosa76@gmail.com>
2022-08-31 17:09:40 +0200

    added snack3

---
+++ snack3.php
@@ -0,0 +1,65 @@
+<?php
+
+$posts_by_date  = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach ($posts_by_date as $date => $posts) { ?>
+        <h1><?php echo $date ?></h1>
+        <?php foreach($posts as $post) {?>
+            <article>
+                <h3><?php echo $post['title'] ?></h3>
+                <p><?php echo $post['text'] ?></p>
+                <address><?php echo $post['author'] ?></address>
+            </article>
+        <?php } ?>   
+    <?php } ?>
+</body>
+</html>
+
